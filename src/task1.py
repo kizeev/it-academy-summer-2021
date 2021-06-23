b7bd6c73c971bb4a8f632364806b3b58c8b531de
kizeev <kizalvic@gmail.com>
2021-06-23 19:11:09 +0300

    hw4: task 1, 2

---
+++ src/task1.py
@@ -0,0 +1,10 @@
+# Dict comprehensions
+# Создайте словарь с помощью генератора словарей, так чтобы его ключами были
+# числа от 1 до 20, а значениями кубы этих чисел.
+
+dictionary = {}
+
+for i in range(1, 21):
+    dictionary[i] = i ** 3
+
+print(dictionary)
