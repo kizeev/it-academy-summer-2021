177ca7b9ce817332ca74abffc5b69163143bf56b
kizeev <kizalvic@gmail.com>
2021-06-24 17:19:50 +0300

    Update task2.py
    hw4: task2

--- src/task2.py
+++ src/task2.py
@@ -10,21 +10,28 @@
 # Для каждого из запроса выведите название страны, в котором находится данный
 # город.
 
-# num_input = int(input('Введите количество стран: '))
-# count = 0
-# list_country = []
-#
-# while count < num_input:
-#     list_country.append(input('Введите страну и ее города: '))
-#     count += 1
+# Создать словарь: ключ = страна, значение = список городов
+num_country = int(input('Введите количество стран: '))
+count1 = 0
+dict_country = {}
 
-list_country = "бел гомель минск горки"
-a = list_country.split()
-b = {}
-for i in a:
-    b[i] =
+while count1 < num_country:
+    country_city = input('Введите страну и ее города: ')
+    dict_country[country_city.split()[0]] = country_city.split()[1:]
+    count1 += 1
 
+# Создать список запрашиваемых городов
+num_city = int(input('Введите количество запросов: '))
+count2 = 0
+list_city = []
 
+while count2 < num_city:
+    city = input('Введите города: ')
+    list_city.append(city)
+    count2 += 1
 
-print(a[0])
-
+# Проверить по каждому городу вхождение в словарь и вывести название страны
+for city in list_city:
+    for country in dict_country:
+        if city in dict_country.get(country):
+            print(country)
