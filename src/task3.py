c20674789e7ffd7c20f3c37f2c52077b6a06305c
kizeev <kizalvic@gmail.com>
2021-07-06 17:24:36 +0300

    Update task3.py

--- src/task3.py
+++ src/task3.py
@@ -3,14 +3,23 @@
 список “сворачивает”.
 """
 
-input_list = [0, 1, 2, 3, 4, 7, 8, 10]
-edit_list = [input_list[0]]
+input_list = [0, 1, 2, 4, 7, 8, 10]
+edit_list = [str(input_list[0])]
 
 for number in range(1, len(input_list)):
-    if edit_list[-1] == input_list[number - 1] and edit_list[-1] != '-':
+    if input_list[number - 1] == input_list[number] - 1 and\
+            input_list[number + 1] == input_list[number] + 1:
+        edit_list.append(',')
+    elif input_list[number - 1] == input_list[number] - 1:
         edit_list.append('-')
+        edit_list.append(str(input_list[number]))
+        edit_list.append(',')
     else:
-        edit_list.append(input_list[number])
-
+        edit_list.append(str(input_list[number]))
+        edit_list.append(',')
 
 print(edit_list)
+
+result_string = ''.join(edit_list)
+print(result_string)
+
