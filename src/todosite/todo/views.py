1fd24e40f6e4e89f088f36fc9c66d4edd3da5cf2
kizeev <kizalvic@gmail.com>
2021-08-31 23:22:24 +0300

    edit index.html

--- src/todosite/todo/views.py
+++ src/todosite/todo/views.py
@@ -6,4 +6,4 @@ def category_task_list(request):
     """Функция отображения категорий и задач на домошней странице сайта."""
     tasks = Task.objects.all()
     categories = CategoryTask.objects.all()
-    return render(request, 'index.html', {'tasks': tasks, 'categories': categories})
+    return render(request, 'index.html', {'tasks': tasks, 'categories': categories, 'title': 'Список задач'})
